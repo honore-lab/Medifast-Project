0fa9098c87e5df103352d4cd93e3ab1276bd8674
honore-lab <h.kamana@alustudent.com>
2026-07-30 11:51:44 +0200

    Strip analytics and clean init

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
@@ -13,10 +12,7 @@ const firebaseConfig = {
   measurementId: "G-SW7H4JDCMZ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-// Export instances for your auth and database logic
 export const auth = getAuth(app);
 export const db = getFirestore(app);
