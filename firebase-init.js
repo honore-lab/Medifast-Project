0877fca31353b7e8d3c65a74a0bc2babcfbd0d9f
honore-lab <h.kamana@alustudent.com>
2026-07-30 11:46:26 +0200

    Export auth and db from firebase-init

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBXApddf0uNHUlBi_mkuFq4dUlUPwHoorM",
-    authDomain: "medi-fast-890e2.firebaseapp.com",
-    projectId: "medi-fast-890e2",
-    storageBucket: "medi-fast-890e2.firebasestorage.app",
-    messagingSenderId: "1015087315721",
-    appId: "1:1015087315721:web:e8babac82dc57474b171d8",
-    measurementId: "G-SW7H4JDCMZ"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBXApddfOuNHUlBi_mkuFq4dUlUpWHoorM",
+  authDomain: "medi-fast-890e2.firebaseapp.com",
+  projectId: "medi-fast-890e2",
+  storageBucket: "medi-fast-890e2.firebasestorage.app",
+  messagingSenderId: "1015087315721",
+  appId: "1:1015087315721:web:e8babac82dc57474b171d8",
+  measurementId: "G-SW7H4JDCMZ"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export auth and db so other files can use them
+export const auth = getAuth(app);
+export const db = getFirestore(app);
